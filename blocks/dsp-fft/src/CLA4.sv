cec1e41ac54920d70fc780a8e52d7899c5303446
Priya Adiga <priyaadiga6@gmail.com>
2026-05-12 00:49:18 +0200

    wallace multiplier but its kind of incorrect

---
+++ blocks/dsp-fft/src/CLA4.sv
@@ -0,0 +1,22 @@
+module CLA4(
+    input logic [3:0] a,
+    input logic [3:0] b, 
+    input logic cin,
+    output logic [3:0] s,  
+    output logic p2, g2
+);
+    logic [3:0] p, g, c;
+
+    assign c[0] = cin; 
+    assign c[1] = g[0] | (p[0] & c[0]);
+    assign c[2] = g[1] | (p[1] & g[0]) | (p[1] & p[0] & c[0]);
+    assign c[3] = g[2] | (p[2] & g[1]) | (p[2] & p[1] & g[0]) | (p[2] & p[1] & p[0] & c[0]);
+
+    full_adder_cla add0(.a(a[0]), .b(b[0]), .cin(c[0]), .p(p[0]), .g(g[0]), .s(s[0]));
+    full_adder_cla add1(.a(a[1]), .b(b[1]), .cin(c[1]), .p(p[1]), .g(g[1]), .s(s[1]));
+    full_adder_cla add2(.a(a[2]), .b(b[2]), .cin(c[2]), .p(p[2]), .g(g[2]), .s(s[2]));
+    full_adder_cla add3(.a(a[3]), .b(b[3]), .cin(c[3]), .p(p[3]), .g(g[3]), .s(s[3]));
+
+    assign g2 = g[3] | (p[3] & g[2]) | (p[3] & p[2] & g[1]) | (p[3] & p[2] & p[1] & g[0]);
+    assign p2 = &p;
+endmodule
--- blocks/dsp-fft/src/fft.sv
+++ blocks/dsp-fft/src/fft.sv
@@ -11,34 +11,64 @@ module fft
     output logic out_valid
 ); 
     
-    logic [15:0] in_re [0:8];
-    logic [15:0] in_im [0:8];
+    // logic [15:0] in_re [0:8];
+    // logic [15:0] in_im [0:8];
 
-    logic valid [0:8]; 
+    // logic valid [0:8]; 
 
-    assign in_re[0] = in_real;
-    assign in_im[0] = in_imag;
+    // assign in_re[0] = in_real;
+    // assign in_im[0] = in_imag;
 
-    assign valid[0] = in_valid;
+    // assign valid[0] = in_valid;
 
-    assign out_real = in_re[8];
-    assign out_imag = in_im[8];
-    assign out_valid = valid[8];
+    // assign out_real = in_re[8];
+    // assign out_imag = in_im[8];
+    // assign out_valid = valid[8];
     
-    genvar i;
-    generate 
-        for (i = 0; i < 8; i++) begin 
-            stage #(
-                .BITS(16), 
-                .STAGES(8), 
-                .CURR_STAGE((i+1)))
-            s (
-                .clk(clk), .rst(rst),
-                .in_valid(valid[i]), 
-                .in_real(in_re[i]), .in_imag(in_im[i]), 
-                .out_real(in_re[i+1]), .out_imag(in_im[i+1]), 
-                .out_valid(valid[i+1])
-            );
+    // genvar i;
+    // generate 
+    //     for (i = 0; i < 8; i++) begin 
+    //         stage #(
+    //             .BITS(16), 
+    //             .STAGES(8), 
+    //             .CURR_STAGE((i+1)))
+    //         s (
+    //             .clk(clk), .rst(rst),
+    //             .in_valid(valid[i]), 
+    //             .in_real(in_re[i]), .in_imag(in_im[i]), 
+    //             .out_real(in_re[i+1]), .out_imag(in_im[i+1]), 
+    //             .out_valid(valid[i+1])
+    //         );
+    //     end
+    // endgenerate
+    logic [15:0] a, b;
+    logic [31:0] c;
+
+    always_ff @(posedge clk, negedge rst) begin 
+        if (!rst) begin 
+            a <= '0; 
+            b <= '0; 
+            out_real <= '0; 
+        end else begin 
+            a <= in_imag;
+            b <= in_real; 
+            out_real <= c;
+
         end
-    endgenerate
+    end
+
+    wallace_mult #(
+     .W(16)
+    ) mult(
+        .clk,
+        .rst, 
+        .a,
+        .b,
+        .p(c)
+    );
+
+    // adder #(.SUB(1)) sub(
+    //     .a(1'b0), .b(b<<<1),
+    //     .result(c)
+    // );
 endmodule
