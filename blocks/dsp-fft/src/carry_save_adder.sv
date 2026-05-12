f712b5927fd6102fc1536f92a6e543eb74702219
Priya Adiga <priyaadiga6@gmail.com>
2026-05-12 20:41:41 +0200

    wallace multiplier works

---
+++ blocks/dsp-fft/src/carry_save_adder.sv
@@ -0,0 +1,15 @@
+module carry_save_adder #(parameter WIDTH) 
+(
+    input logic [WIDTH-1:0] a, 
+    input logic [WIDTH-1:0] b, 
+    input logic [WIDTH-1:0] cin,
+    output logic [WIDTH-1:0] s, 
+    output logic [WIDTH-1:0] cout  
+);
+
+    generate
+        for (genvar i = 0; i < WIDTH; i++) begin 
+            full_adder fa(.a(a[i]), .b(b[i]), .cin(cin[i]), .s(s[i]), .cout(cout[i]));
+        end
+    endgenerate
+endmodule
